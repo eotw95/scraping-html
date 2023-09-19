2b32a22a0275b9c2b80cfbbfbec3abde14f10974
RYUJIKOYAMANO\ryuji <ryujikoyama95@gmail.com>
2023-09-19 22:42:29 +0900

    Fix code

--- app/src/main/java/com/example/scraping_html/MainActivity.kt
+++ app/src/main/java/com/example/scraping_html/MainActivity.kt
@@ -2,21 +2,15 @@ package com.example.scraping_html
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.widget.TextView
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.URL
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var handler: Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // UIスレッドのHandlerを作成
-        handler = Handler(Looper.getMainLooper())
 
         // バックグラウンドスレッドでWebページを取得し、タイトル文字をリストで取得
         Thread {
